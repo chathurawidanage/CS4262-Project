6eeb7357f9fe2028a5b9e87d3829d8b1b31f79ad
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-05 23:27:08 +0530

    Search node basic implementation

--- src/main/java/lk/ac/mrt/distributed/SearchNode.java
+++ src/main/java/lk/ac/mrt/distributed/SearchNode.java
@@ -1,49 +1,59 @@
 package lk.ac.mrt.distributed;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.PriorityQueue;
+import lk.ac.mrt.distributed.api.Node;
+import lk.ac.mrt.distributed.api.NodeOps;
+import lk.ac.mrt.distributed.api.exceptions.BootstrapException;
+import lk.ac.mrt.distributed.api.exceptions.CommunicationException;
+import lk.ac.mrt.distributed.api.exceptions.NullCommandListenerException;
+import lk.ac.mrt.distributed.api.messages.responses.RegisterResponse;
+import lk.ac.mrt.distributed.api.messages.responses.UnRegisterResponse;
+
+import java.net.*;
+import java.util.*;
 
 /**
  * @author Chathura Widanage
  */
-public class SearchNode extends PriorityQueue implements Runnable {
-    private DatagramSocket socket;
+public class SearchNode extends Node implements CommandListener {
+    private Set<Node> neighbours;
+    private Map<String, Node> masters;
+    private Map<String, List<Node>> resourceProviders;
 
-    public SearchNode() throws SocketException {
-        socket = new DatagramSocket(4444);
+    private NodeOps nodeOps;
+
+
+    public SearchNode(String myIp, int myPort, NodeOps nodeOps) throws SocketException, NullCommandListenerException, BootstrapException {
+        super(myIp, myPort);
+        this.neighbours = new HashSet<>();
+        this.masters = new HashMap<>();
+        this.resourceProviders = new HashMap<>();
+
+        this.nodeOps = nodeOps;
+        this.nodeOps.setCommandListener(this);
+        this.nodeOps.start(this);
     }
 
+    public void bootstrap() throws SocketException, UnknownHostException, CommunicationException {
+        //register node
+        RegisterResponse registerResponse = nodeOps.register();
+        this.neighbours.addAll(registerResponse.getNodes());
+
+        this.nodeOps.join(this.neighbours);
+    }
+
+    //listening forever
+    //todo move to a different class
     public void run() {
-        byte buffer[];
-        DatagramPacket datagramPacket;
-        while (true) {
-            buffer = new byte[65536];
-            datagramPacket = new DatagramPacket(buffer, buffer.length);
-            try {
-                System.out.println("Waiting...");
-                socket.receive(datagramPacket);
-                System.out.println(new String(buffer).trim());
-                System.out.println(datagramPacket.getAddress().toString() + ":" + datagramPacket.getPort());
-                send(datagramPacket.getAddress(),
-                        datagramPacket.getPort(), "GOT".getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
     }
 
     private void receive(DatagramPacket datagramPacket) {
 
     }
 
-    private void send(InetAddress inetAddress, int port, byte[] msg) throws IOException {
-        DatagramPacket datagramPacket = new DatagramPacket(msg, msg.length);
-        datagramPacket.setAddress(inetAddress);
-        datagramPacket.setPort(port);
-        socket.send(datagramPacket);
+
+    @Override
+    public void onSearchRequest(Node node, String keyword) {
+
     }
 }
