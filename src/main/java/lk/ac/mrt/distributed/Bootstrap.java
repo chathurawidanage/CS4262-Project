958765215c6b1b205ad71ea76eddadebb80dfff7
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-05 23:27:57 +0530

    Bootstrap node  implementation

--- src/main/java/lk/ac/mrt/distributed/Bootstrap.java
+++ src/main/java/lk/ac/mrt/distributed/Bootstrap.java
@@ -1,12 +1,29 @@
 package lk.ac.mrt.distributed;
 
+import lk.ac.mrt.distributed.api.exceptions.BootstrapException;
+import lk.ac.mrt.distributed.api.exceptions.CommunicationException;
+import lk.ac.mrt.distributed.api.exceptions.NullCommandListenerException;
+
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * @author Chathura Widanage
  */
 public class Bootstrap {
     public static void main(String[] args) throws SocketException {
-        new Thread(new SearchNode()).start();
+        NodeOpsUDPImpl nodeOpsUDP = new NodeOpsUDPImpl("127.0.0.1", 55555);
+        try {
+            SearchNode searchNode = new SearchNode("127.0.0.1", 44444, nodeOpsUDP);
+            searchNode.bootstrap();
+        } catch (NullCommandListenerException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (BootstrapException e) {
+            e.printStackTrace();
+        } catch (CommunicationException e) {
+            e.printStackTrace();
+        }
     }
 }
