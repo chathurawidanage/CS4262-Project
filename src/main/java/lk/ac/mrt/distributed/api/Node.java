ab701203823e6db29142ec5b574cf3c456c9127d
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-05 20:37:55 +0530

    Implementing equals and hashcode for Node

--- src/main/java/lk/ac/mrt/distributed/api/Node.java
+++ src/main/java/lk/ac/mrt/distributed/api/Node.java
@@ -7,9 +7,41 @@ import java.util.Map;
  * @author Chathura Widanage
  */
 public class Node {
-    private int port;
-    private String ip;
-    private List<Node> neighbours;
-    private Map<String, Node> masters;
-    private Map<String, List<Node>> resourceProviders;
+    protected int port;
+    protected String ip;
+
+    public Node(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null && obj instanceof Node) {
+            Node comp = (Node) obj;
+            return this.ip.equals(comp) && this.port == comp.port;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (ip + ":" + port).hashCode();
+    }
 }
