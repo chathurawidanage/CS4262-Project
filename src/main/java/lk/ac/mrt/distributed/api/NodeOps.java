60d5f9ad2c041c8500e6d188a689ea68a818fd52
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-05 20:39:02 +0530

    NodeOps interface definition

--- src/main/java/lk/ac/mrt/distributed/api/NodeOps.java
+++ src/main/java/lk/ac/mrt/distributed/api/NodeOps.java
@@ -3,17 +3,31 @@ package lk.ac.mrt.distributed.api;
 import lk.ac.mrt.distributed.api.messages.responses.RegisterResponse;
 import lk.ac.mrt.distributed.api.messages.responses.UnRegisterResponse;
 
+import java.util.Set;
+
 /**
  * @author Chathura Widanage
  */
 public interface NodeOps {
+    /**
+     * Registers in the network by communicating with the bootstrap server
+     *
+     * @param node Node to be registered
+     * @return {@link RegisterResponse} instance
+     */
     RegisterResponse register(Node node);
 
+    /**
+     * Unregisters node from the network
+     *
+     * @param node
+     * @return
+     */
     UnRegisterResponse unregister(Node node);
 
-    void join(Node node);
+    void join(Node node, Set<Node> neighbours);
 
-    void leave(Node node);
+    void leave(Node node, Set<Node> neighbours);
 
-    void search(Node node, String fileName);
+    void search(Node node, String fileName, Set<Node> neighbours);
 }
