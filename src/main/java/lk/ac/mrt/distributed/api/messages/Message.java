700c9302bed4e6a7a543766e42b302e1be4493e9
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-07 15:03:06 +0530

    Message class modified

--- src/main/java/lk/ac/mrt/distributed/api/messages/Message.java
+++ src/main/java/lk/ac/mrt/distributed/api/messages/Message.java
@@ -6,17 +6,7 @@ package lk.ac.mrt.distributed.api.messages;
 public abstract class Message {
     public abstract String getSendableString();
 
-    public static String getLengthAppenedMessage(String message){
-        int length = message.length() + 5;
-        String appened = "";
-        if(length<1000){
-            appened+="0";
-        }else if(length<100){
-            appened+="00";
-        }else if(length<10){
-            appened+="000";
-        }
-        appened+=length+" "+message;
-        return appened;
+    public static String getLengthAppenedMessage(String message) {
+        return String.format("%04d", message.length() + 5) + " " + message;
     }
 }
