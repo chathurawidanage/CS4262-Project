c86fb7e633a5cedab6c0ff011c14d37c938c7508
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-07 19:57:28 +0530

    Minor updates

--- src/main/java/lk/ac/mrt/distributed/Bootstrap.java
+++ src/main/java/lk/ac/mrt/distributed/Bootstrap.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * @author Chathura Widanage
@@ -18,7 +18,6 @@ public class Bootstrap {
     private static final Logger logger = LogManager.getLogger(Bootstrap.class);
 
     public static void main(String[] args) throws SocketException {
-
         NodeOpsUDPImpl nodeOpsUDP = new NodeOpsUDPImpl("127.0.0.1", 55555);
         try {
             SearchNode searchNode = new SearchNode("NODE1", "127.0.0.1", 44444, nodeOpsUDP);
