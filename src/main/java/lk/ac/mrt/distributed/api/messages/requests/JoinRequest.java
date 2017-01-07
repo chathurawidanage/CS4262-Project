05beb671c048d4f815a46858f5201d9ed30f6488
Chathura Widanage <chathura.12@cse.mrt.ac.lk>
2017-01-07 14:51:18 +0530

    JoinRequest done

---
+++ src/main/java/lk/ac/mrt/distributed/api/messages/requests/JoinRequest.java
@@ -0,0 +1,42 @@
+package lk.ac.mrt.distributed.api.messages.requests;
+
+import lk.ac.mrt.distributed.api.Node;
+import lk.ac.mrt.distributed.api.messages.Message;
+
+import java.util.StringTokenizer;
+
+/**
+ * @author Chathura Widanage
+ */
+public class JoinRequest implements Message {
+    private Node node;
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+
+    public static JoinRequest parse(String msg) {
+        StringTokenizer stringTokenizer = new StringTokenizer(msg, " ");
+        String length = stringTokenizer.nextToken();
+        String message = stringTokenizer.nextToken();
+        String ip = stringTokenizer.nextToken();
+        Integer port = Integer.parseInt(stringTokenizer.nextToken());
+
+        Node node = new Node(ip, port);
+        JoinRequest joinRequest = new JoinRequest();
+        joinRequest.setNode(node);
+        return joinRequest;
+    }
+
+    @Override
+    public String getSendableString() {
+        Node node = this.getNode();
+        String msg = "JOIN " + node.getIp() + " " + node.getPort();
+        msg = msg.length() + " " + msg;
+        return msg;
+    }
+}
