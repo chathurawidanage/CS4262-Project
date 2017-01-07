99d10045e5a7347a428b78af4817b8af3b5570fd
mdslasantha <mdslasantha@gmail.com>
2017-01-07 14:54:27 +0530

    getLengthAppenedMessage added

--- src/main/java/lk/ac/mrt/distributed/api/messages/Message.java
+++ src/main/java/lk/ac/mrt/distributed/api/messages/Message.java
@@ -3,6 +3,20 @@ package lk.ac.mrt.distributed.api.messages;
 /**
  * @author Chathura Widanage
  */
-public interface Message {
-    String getSendableString();
+public abstract class Message {
+    abstract String getSendableString();
+
+    public static String getLengthAppenedMessage(String message){
+        int length = message.length() + 5;
+        String appened = "";
+        if(length<1000){
+            appened+="0";
+        }else if(length<100){
+            appened+="00";
+        }else if(length<10){
+            appened+="000";
+        }
+        appened+=length+" "+message;
+        return appened;
+    }
 }
--- src/main/java/lk/ac/mrt/distributed/api/messages/requests/UnresgiterRequest.java
+++ src/main/java/lk/ac/mrt/distributed/api/messages/requests/UnresgiterRequest.java
@@ -9,6 +9,10 @@ public class UnresgiterRequest implements Message {
 
 
     public String getSendableString() {
+        // length UNREG IP_address port_no username
+
+        //0028 UNREG 64.12.123.190 432
+        String unregMessage =
         return null;
     }
 }
